ed9ae322413099bcdd04cf4ce9eef577b1740552
Ayesha Rustam <ayeshykhan47@gmail.com>
2026-02-17 21:33:09 +0500

    Create function to check prime number.cpp

---
+++ function to check prime number.cpp
@@ -0,0 +1,27 @@
+//Function to check prime number
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if(n <= 1)
+        return false;
+    for(int i = 2; i*i <= n; i++) {
+        if(n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if(isPrime(num))
+        cout << num << " is Prime" << endl;
+    else
+        cout << num << " is Not Prime" << endl;
+
+    return 0;
+}
